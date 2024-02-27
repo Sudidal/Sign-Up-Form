33067c346471932e33b9b9ec9f0698a3c5d07369
Oxide <mohammedhaider15mo@gmail.com>
2024-02-27 19:57:40 +0300

    Add a message field for the form There wasn't a way to communicate with the user in the field so i added a message div to hold the errors and styled it

--- script.js
+++ script.js
@@ -6,24 +6,31 @@ const passInput = document.querySelector("#passinput")
 const confirmPassInput = document.querySelector("#confirmpassinput")
 const submitBtn = document.querySelector("#submitbtn")
 const message = document.querySelector("#form-message")
+const messageText = document.querySelector("#message-text")
+const crossIcon = document.querySelector("#crosicon")
+const sucIcon = document.querySelector("#sucicon")
 
 submitBtn.addEventListener("click", checkFormValidity);
+message.style = "display: none"
 
 function checkFormValidity()
 {
-    if(notEmpty(firstNameInput) && 
-    notEmpty(lastNameInput) && notEmpty(emailInput) 
-    && notEmpty(telInput) && notEmpty(passInput) && 
-    notEmpty(confirmPassInput))
+    if(isValid(firstNameInput) && 
+    isValid(lastNameInput) && isValid(emailInput) 
+    && isValid(telInput) && isValid(passInput) && 
+    isValid(confirmPassInput))
     {
         if(passwordValidation())
         {
-            console.log("Submitted");
+            submit();
+            successMessage();
         }
     }
     else
     {
         console.log("Please fill out all fields");
+        messageText.innerText = "Please fill out all fields!"
+        invalidMessage();
     }
 }
 
@@ -36,13 +43,37 @@ function passwordValidation()
     }
     else
     {
-        confirmPassInput.setCustomValidity("Passwords do not match!");
         console.log("Passwords do not match!");
+        messageText.innerText = "Passwords do not match!";
+        invalidMessage();
         return false;
     }
 }
 
-function notEmpty(input)
+function isValid(input)
 {
-    return input.value != "";
+    return input.value != "" && input.checkValidity();
+}
+
+function submit()
+{
+    console.log("Submitted!");
+    messageText.innerText = "Submitted!"
+    message.style = "display: flex"
+    sucIcon.style = "display: flex"
+    crossIcon.style = "display: none"
+}
+function invalidMessage()
+{
+    messageText.style = "Color: red"
+    message.style = "display: flex"
+    sucIcon.style = "display: none"
+    crossIcon.style = "display: flex"
+}
+function successMessage()
+{
+    messageText.style = "Color: green"
+    message.style = "display: flex"
+    sucIcon.style = "display: flex"
+    crossIcon.style = "display: none"
 }
